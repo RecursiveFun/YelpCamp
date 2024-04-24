e8b63a72cec7859032cccdb436bb991745d74d29
FelixBerinde <Felixthepuppy@yahoo.com>
2024-04-23 23:25:44 -0400

    Adding basic routes, views, and seed data

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require ('express');
 const path = require('path');
 const mongoose = require('mongoose');
+const Campground = require('./models/campground')
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp');
 
@@ -18,6 +19,16 @@ app.set('views', path.join(__dirname, 'views'))
 app.get('/', (req, res) => {
     res.render('home')
 });
+
+app.get('/camps', async (req, res) => {
+    const camps = await Campground.find({});
+    res.render('camps/index', { camps })
+});
+
+app.get('/camps/:id', async (req, res) => {
+    const camp = await Campground.findById(req.params.id)
+    res.render('camps/show', { camp });
+})
 
 app.listen(3000, () => {
     console.log("Serving on port 3000")
