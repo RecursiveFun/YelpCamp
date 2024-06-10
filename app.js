ffdfd4556651b45aade13cf4cf5be8dfed05a220
RecursiveFun <felixthepuppy@yahoo.com>
2024-06-10 02:43:35 -0400

    Fixed bad routes bug by checking to see if the objectId is valid before trying to use it.

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ const ExpressError = require('./utils/ExpressError');
 const methodOverride = require('method-override');
 const camps = require('./routes/camps');
 const reviews = require('./routes/reviews');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const User = require('./models/user');
 
 
 
@@ -41,13 +44,28 @@ const sessionConfig = {
         maxAge: 1000 * 60 * 60 * 24 * 7
     }
 }
+
 app.use(session(sessionConfig))
 app.use(flash());
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.use((req, res, next) => {
     res.locals.success = req.flash('success');
     res.locals.error = req.flash('error');
     next();
+})
+
+app.get('/fakeuser', async (req, res) =>{
+    const user = new User({email: 'fberind@wgu.edu', username: 'felix'})
+    const newUser = await User.register(user, 'password');
+    res.send(newUser);
 })
 
 app.use('/camps', camps);
