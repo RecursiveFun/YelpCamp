4120718981ca237874ba3077419c8758a1d9bb23
Felix <50165092+RecursiveFun@users.noreply.github.com>
2026-06-14 21:22:05 -0400

    Bug fix

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const catchAsync = require('../utils/catchAsync');
 const User = require('../models/user');
-const { storeReturnTo } = require('../middleware');
+const { storeReturnTo } = require('../middlewares');
 const passport = require('passport');
 const users = require('../controllers/users');
 
